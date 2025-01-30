8d002d748e94a2fda0102c007f6cae7bb2824f16
Eman-Nadeem <webdev.eman@gmail.com>
2025-01-30 17:00:19 +0500

    Handled most of the message sending and recieving functionality

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import { connnectDB } from "./lib/connnect.db.js";
 import cookieParser from "cookie-parser";
 
@@ -14,6 +15,7 @@ app.use(express.json()); //extract json data from body
 app.use(cookieParser()); //parse cookies
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
